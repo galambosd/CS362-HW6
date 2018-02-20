9eba15fd0336ef2ddf34398581c735f19603ec04
David Galambos <31393112+galambosd@users.noreply.github.com>
2018-02-20 17:02:52 -0600

    Read reads and create k-mers

--- assemble.py
+++ assemble.py
@@ -1,7 +1,7 @@
 import sys
 
 try:
-    reads = open(sys.argv[1]."r")
+    readFile = open(sys.argv[1],"r")
     k = int(sys.argv[2])
 except(IndexError):
     print("Too few arguments found. Assemble requires a txt reads file and a k-mer length.")
@@ -12,6 +12,25 @@ except(FileNotFoundError):
 except(ValueError):
     print("K-mer length must be an integer.")
     exit(1)
+
+reads = []
+# read in the reads
+for line in readFile.readlines():
+    reads.append(line.rstrip('\n').upper())
+# check that k isn't longer than reads
+# assuming all reads same length
+if k > len(reads[0]):
+    print("You tried to create k-mers that are longer than your reads.")
+    exit(1)
+# build set of all k-mers
+kMers = []
+for read in reads:
+    for i in range(0, len(read)-k):
+        kMers.append(read[i:i+k])
+
+print(kMers)
+# build (k-1)mers?
+# create a class for our DB Graph?
 
 
 
