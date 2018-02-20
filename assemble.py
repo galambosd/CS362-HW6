2c25e1725ae6f9a113f7be1abbe3545fd2567981
Blake Johnson <blakeconnorjohnson@gmail.com>
2018-02-20 17:38:20 -0600

    added dot file functionality

--- assemble.py
+++ assemble.py
@@ -31,8 +31,29 @@ for read in reads:
 # build (k-1)mers?
 # create a class for our DB Graph?
 
+graph = {}
+for kMer in kMers:
+    k1Mer = kMer[:-1]
+    k2Mer = kMer[1:]
+    if k1Mer in graph.keys():
+        graph[k1Mer].append(k2Mer)
+    else:
+        graph[k1Mer] = [k2Mer]
+    if k2Mer not in graph.keys():
+        graph[k2Mer] = []
+        
+#We're gonna visualize this with dot now.
 
 
+with open("graph.dot","w") as dotFile:
+    dotFile.write("digraph DeBruijn{\n")
+    for key in graph.keys():
+        for value in graph[key]:
+            dotFile.write("\t\"" + key + "\"->\"" + value + "\"\n")
+    dotFile.write("}")
+    dotFile.close()
+
+        
 
 with open("contigs.txt","w") as output:
     for arg in sys.argv:
--- assemble.sh
+++ assemble.sh
@@ -2,3 +2,5 @@
 
 # $@ will pass all parameters you passed to the shell script to the python script below
 python3 assemble.py $@
+
+dot -Tpng graph.dot -o DeBruijnGraph.png
