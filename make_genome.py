b26156a361849fb10414498c0a472e3d4b347401
Blake Johnson <blakeconnorjohnson@gmail.com>
2018-02-27 20:05:25 -0600

    make genome is a thing now

---
+++ make_genome.py
@@ -0,0 +1,17 @@
+from sys import argv
+from random import randint
+
+length = argv[1]
+
+nucleotides = ['A', 'C', 'T', 'G']
+
+genome = ''
+for i in range(0, int(length)):
+    genome+= nucleotides[randint(0,3)]
+
+with open('simple.fasta', 'w') as outFile:
+    outFile.write('>Genome generated with make_genome.py\n')
+    outFile.write(genome+'\n')
+    outFile.close()
+
+print('Done.')
