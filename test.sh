181468e16bdaabdef881eec1bdf2ab1af46aa24f
Blake Johnson <blakeconnorjohnson@gmail.com>
2018-02-28 21:24:18 -0600

    adding our output and reads for sample.fasta

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ for i in $(seq $2)
 do
 	python3 make_genome.py $1 >/dev/null
 	python3 simulate.py simple.fasta 25 150 .05 >/dev/null
-	python3 assemble.py reads.txt 19 >/dev/null
+	python3 assemble.py reads.txt 13 >/dev/null
 	python3 test_assembly.py simple.fasta contigs.txt
 done
 exit 0
